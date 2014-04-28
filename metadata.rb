cd32ca792151958514d30402900ee70c27931a37
Koji Tanaka <kj.tanaka@gmail.com>
2014-04-27 21:49:50 -0400

    update

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'openstack-keystone'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Koji Tanaka'
+maintainer_email 'kj.tanaka@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures openstack-keystone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'database'
